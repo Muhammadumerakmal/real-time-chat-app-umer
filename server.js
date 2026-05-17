24529eb5b81429dcb837398b4a62e285fe4b1601
Muhammadumerakmal <umeraura99@gmail.com>
2026-05-17 17:27:48 +0500

    going forward to get it

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 import express from "express"
+import {createServer} from "node:http"
+import {Server} from "socket.io"
 
+
+// -------------------------------------------------------------------------------------------------------------------------------------//
 const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer,{
+    cors:{
+        origin:"*"
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log(" ✅ user connected", socket.id);
+
+    socket.on("message",(msg)=>{
+        // send to everyone
+        io.emit("message",msg)
+    })
+      
+    socket.on("disconnect",()=>{
+        console.log("❌ user disconnected", socket.id);
+    })
+})
+
+
 
 app.listen(3000,()=>{
     console.log("server running on port 3000");
