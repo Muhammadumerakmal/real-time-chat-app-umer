f72119886bd1ad94047771387072e7cad10d3cee
Muhammadumerakmal <umeraura99@gmail.com>
2026-05-21 21:09:53 +0500

    part 2 done

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-import express from "express"
-import {createServer} from "node:http"
-import {Server} from "socket.io"
-
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 // -------------------------------------------------------------------------------------------------------------------------------------//
-const app = express()
-const httpServer = createServer(app)
-const io = new Server(httpServer,{
-    cors:{
-        origin:"*"
-    }
-})
+const app = express();
 
-io.on("connection",(socket)=>{
-    console.log(" ✅ user connected", socket.id);
+// serve html files
+app.use(express.static("public"));
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
-    socket.on("message",(msg)=>{
-        // send to everyone
-        io.emit("message",msg)
-    })
-      
-    socket.on("disconnect",()=>{
-        console.log("❌ user disconnected", socket.id);
-    })
-})
+io.on("connection", (socket) => {
+  console.log(" ✅ user connected", socket.id);
 
+  socket.on("message", (msg) => {
+    // send to everyone (emit = event use)
+    io.emit("message", msg);
+  });
 
+  socket.on("disconnect", () => {
+    console.log("❌ user disconnected", socket.id);
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("server running on port 3000 🔌");
-    
-})
+app.listen(3000, () => {
+  console.log("server running on port 3000 🔌");
+});
